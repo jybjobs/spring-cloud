e5733d82566930943149247f08719c01828e2e2a
jinyubing <jinyubing@yihecloud.com>
2018-04-18 14:17:13 +0800

    增加 springcloud admin.

--- eureka/eureka-server/src/main/java/com/yihe/eureka/EurekaServerApplication.java
+++ eureka/eureka-server/src/main/java/com/yihe/eureka/EurekaServerApplication.java
@@ -1,10 +1,11 @@
 package com.yihe.eureka;
 
+import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableCloudDashboard
 @EnableEurekaServer //开启 eureka-server
 @SpringBootApplication
 public class EurekaServerApplication {
